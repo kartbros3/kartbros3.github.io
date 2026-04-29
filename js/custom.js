d826c5de93daafedd10fa6a54ca74d9827dc6e7f
Mubeen Ilyas <youremail@example.com>
2026-04-29 19:48:11 +0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+function loadGoogleAnalytics(trackingId) {
+  // Create the script tag for gtag.js
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
+  document.head.appendChild(script);
+
+  // Initialize the dataLayer and gtag function
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  // Assign gtag function globally
+  window.gtag = gtag;
+
+  // Configure gtag
+  gtag('js', new Date());
+  gtag('config', trackingId);
+}
+
+// Usage
+loadGoogleAnalytics('G-4TK4BSTE7K');
+/*
+
+Custom script
+
+This file will not be overwritten by the updater
+
+*/
+
+// JavaScript code
+function search_animal() {
+  let input = document.getElementById("searchbar").value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("animals");
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "block";
+    }
+  }
+}
